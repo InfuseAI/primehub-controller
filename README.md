a8eea4d7af9347678ae0dec0eee98697353e6c12
Timothy Lee <timmy.ccl@gmail.com>
2019-10-31 13:36:34 +0800

    Add scripts and update README

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# primehub-controller
+# PrimeHub Controller
+
+## Custom Image
+
+### Config Map
+
+1. Add build-and-push script to ConfigMap
+
+    ```
+    kubectl -n hub create configmap script-build-and-push --from-file ./scripts/custom_image/build-and-push.sh
+    ```
+
+### Push Secret
+
+1. Create push secret for container registry, all built images will be pushed using this secret.
+
+    ```
+    kubectl -n hub create secret docker-registry $PUSH_SECRET_NAME --docker-server=$DOCKER_SERVER --docker-username=$DOCKER_USERNAME --docker-password="$DOCKER_PASSWORD"
+    ```
+
+### Environment Variables
+
+* `BUILD_IMAGE_SECRET_NAME`: $PUSH_SECRET_NAME
+* `BUILD_IMAGE_REPO_PREFIX`: Prefix for image. (e.g. 'gcr.io/infuseai' for 'gcr.io/infuseai/primehub-controller:be20ee69c5')
---
+++ scripts/custom_image/build-and-push.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+readonly INIT_FAILED=1
+readonly BUILD_FAILED=2
+readonly PUSH_FAILED=3
+
+readonly PUSH_SECRET_AUTHFILE=/push-secret/.dockerconfigjson
+readonly PULL_SECRET_AUTHFILE=/pull-secret/.dockerconfigjson
+
+function init() {
+  echo "[Step: init]"
+  echo "Generating Dockerfile"
+  echo "$DOCKERFILE" > /Dockerfile
+  [[ $? -ne 0 ]] && { exit $INIT_FAILED; }
+  echo
+}
+
+function build() {
+  local -a AUTFILE_FLAGS
+  echo "[Step: build]"
+  echo "Fetching base image: $BASE_IMAGE"
+  if [ -f $PULL_SECRET_AUTHFILE ]; then
+    AUTHFILE_FLAGS+=(--authfile "$PULL_SECRET_AUTHFILE")
+  fi
+  buildah --storage-driver vfs "${AUTHFILE_FLAGS[@]}" pull $BASE_IMAGE
+  echo
+  echo "Building image: $TARGET_IMAGE"
+  buildah --storage-driver vfs bud -f /Dockerfile -t $TARGET_IMAGE .
+  [[ $? -ne 0 ]] && { exit $BUILD_FAILED; }
+  echo
+}
+
+function push() {
+  echo "[Step: push]"
+  buildah --storage-driver vfs --authfile $PUSH_SECRET_AUTHFILE push $TARGET_IMAGE
+  [[ $? -ne 0 ]] && { exit $PUSH_FAILED; }
+  echo
+}
+
+function completed() {
+  echo "[Completed]"
+  buildah --storage-driver vfs images
+  echo
+}
+
+init
+build
+push
+completed
