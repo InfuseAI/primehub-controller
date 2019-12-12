f6ce025166838fa2fd1e4f625fc946b3e0d4896c
Jack Pan <jackpan@infuseai.io>
2019-12-12 10:01:53 +0800

    update unit test

--- controllers/phjob_scheduler_test.go
+++ controllers/phjob_scheduler_test.go
@@ -4,8 +4,6 @@ import (
 	"testing"
 	"time"
 
-	"k8s.io/apimachinery/pkg/api/resource"
-
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
@@ -13,24 +11,8 @@ import (
 	"primehub-controller/pkg/graphql"
 )
 
-func createResourceQuota(cpu int64, gpu int64, memory string) *ResourceQuota {
-	resourceQuota := NewResourceQuota()
-	if cpu >= 0 {
-		resourceQuota.cpu = resource.NewQuantity(cpu, resource.DecimalSI)
-	} else {
-		resourceQuota.cpu = nil
-	}
-	if gpu >= 0 {
-		resourceQuota.gpu = resource.NewQuantity(gpu, resource.DecimalSI)
-	} else {
-		resourceQuota.gpu = nil
-	}
-	if memory != "" {
-		temp := resource.MustParse(memory)
-		resourceQuota.memory = &temp
-	} else {
-		resourceQuota.memory = nil
-	}
+func getConvertedResourceQuota(cpu float32, gpu float32, memory string) *ResourceQuota {
+	resourceQuota, _ := ConvertToResourceQuota(cpu, gpu, memory)
 	return resourceQuota
 }
 
@@ -81,37 +63,37 @@ func TestValidate(t *testing.T) {
 	phJobScheduler := PHJobScheduler{}
 
 	groupInfo := createGroupInfo("groupA", 3, -1, "", 5, -1, "")
-	requestedQuota := createResourceQuota(4, 1, "1Gi")
+	requestedQuota, _ := ConvertToResourceQuota(4, 1, "1Gi")
 	valid, _ := phJobScheduler.validate(requestedQuota, groupInfo)
 	if valid != false {
 		t.Error("Wrong Validate of Group Quota Test 1")
 	}
-	requestedQuota = createResourceQuota(3, 1, "1Gi")
+	requestedQuota, _ = ConvertToResourceQuota(3, 1, "1Gi")
 	valid, _ = phJobScheduler.validate(requestedQuota, groupInfo)
 	if valid != true {
 		t.Error("Wrong Validate of Group Quota Test 2")
 	}
 
 	groupInfo = createGroupInfo("groupA", 5, -1, "", 3, -1, "")
-	requestedQuota = createResourceQuota(4, 1, "1Gi")
+	requestedQuota, _ = ConvertToResourceQuota(4, 1, "1Gi")
 	valid, _ = phJobScheduler.validate(requestedQuota, groupInfo)
 	if valid != false {
 		t.Error("Wrong Validate of User Quota Test 1")
 	}
-	requestedQuota = createResourceQuota(3, 1, "1Gi")
+	requestedQuota, _ = ConvertToResourceQuota(3, 1, "1Gi")
 	valid, _ = phJobScheduler.validate(requestedQuota, groupInfo)
 	if valid != true {
 		t.Error("Wrong Validate of User Quota Test 2")
 	}
 
 	groupInfo = createGroupInfo("groupA", -1, -1, "0.5Gi", 3, -1, "")
-	requestedQuota = createResourceQuota(1, 1, "1Gi")
+	requestedQuota, _ = ConvertToResourceQuota(1, 1, "1Gi")
 	valid, _ = phJobScheduler.validate(requestedQuota, groupInfo)
 	if valid != false {
 		t.Error("Wrong Validate of Memory Quota Test 1")
 	}
 	groupInfo = createGroupInfo("groupA", -1, -1, "1Gi", 3, -1, "")
-	requestedQuota = createResourceQuota(1, 1, "1Gi")
+	requestedQuota, _ = ConvertToResourceQuota(1, 1, "1Gi")
 	valid, _ = phJobScheduler.validate(requestedQuota, groupInfo)
 	if valid != true {
 		t.Error("Wrong Validate of Memory Quota Test 2")
@@ -172,11 +154,11 @@ func TestScheduleByStrictOrder1(t *testing.T) {
 	phJobs = append(phJobs, createPhJob("userC", "groupA", "typeA"))
 
 	usersRemainingQuota := make(map[string]ResourceQuota)
-	usersRemainingQuota["userA"] = *createResourceQuota(8, -1, "")
-	usersRemainingQuota["userB"] = *createResourceQuota(8, -1, "")
-	usersRemainingQuota["userC"] = *createResourceQuota(8, -1, "")
+	usersRemainingQuota["userA"] = *getConvertedResourceQuota(8, -1, "")
+	usersRemainingQuota["userB"] = *getConvertedResourceQuota(8, -1, "")
+	usersRemainingQuota["userC"] = *getConvertedResourceQuota(8, -1, "")
 
-	groupRemainingQuota := createResourceQuota(8, -1, "")
+	groupRemainingQuota := getConvertedResourceQuota(8, -1, "")
 
 	instanceTypeInfo := createInstanceTypeInfo("typeA", 4, 0, "")
 	InstanceTypeCache.Set("instanceType:typeA", instanceTypeInfo, time.Hour*1)
@@ -197,11 +179,11 @@ func TestScheduleByStrictOrder2(t *testing.T) {
 	phJobs = append(phJobs, createPhJob("userC", "groupA", "typeC"))
 
 	usersRemainingQuota := make(map[string]ResourceQuota)
-	usersRemainingQuota["userA"] = *createResourceQuota(8, -1, "")
-	usersRemainingQuota["userB"] = *createResourceQuota(8, -1, "")
-	usersRemainingQuota["userC"] = *createResourceQuota(8, -1, "")
+	usersRemainingQuota["userA"] = *getConvertedResourceQuota(8, -1, "")
+	usersRemainingQuota["userB"] = *getConvertedResourceQuota(8, -1, "")
+	usersRemainingQuota["userC"] = *getConvertedResourceQuota(8, -1, "")
 
-	groupRemainingQuota := createResourceQuota(8, -1, "")
+	groupRemainingQuota := getConvertedResourceQuota(8, -1, "")
 
 	typeAInfo := createInstanceTypeInfo("typeA", 4, 0, "")
 	InstanceTypeCache.Set("instanceType:typeA", typeAInfo, time.Hour*1)
@@ -226,11 +208,11 @@ func TestScheduleByStrictOrder3(t *testing.T) {
 	phJobs = append(phJobs, createPhJob("userC", "groupA", "typeC"))
 
 	usersRemainingQuota := make(map[string]ResourceQuota)
-	usersRemainingQuota["userA"] = *createResourceQuota(4, -1, "")
-	usersRemainingQuota["userB"] = *createResourceQuota(4, -1, "")
-	usersRemainingQuota["userC"] = *createResourceQuota(4, -1, "")
+	usersRemainingQuota["userA"] = *getConvertedResourceQuota(4, -1, "")
+	usersRemainingQuota["userB"] = *getConvertedResourceQuota(4, -1, "")
+	usersRemainingQuota["userC"] = *getConvertedResourceQuota(4, -1, "")
 
-	groupRemainingQuota := createResourceQuota(8, -1, "")
+	groupRemainingQuota := getConvertedResourceQuota(8, -1, "")
 
 	typeAInfo := createInstanceTypeInfo("typeA", 4, 0, "")
 	InstanceTypeCache.Set("instanceType:typeA", typeAInfo, time.Hour*1)
