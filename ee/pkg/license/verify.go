66d9f981d95d6b62a85b65050fa2cb7ed5827636
Timothy Lee <timmy.ccl@gmail.com>
2020-01-14 18:02:03 +0800

    Add default signed license and extract const variables
    Signed-off-by: Timothy Lee <ctiml@infuseai.io>

--- ee/pkg/license/verify.go
+++ ee/pkg/license/verify.go
@@ -11,16 +11,6 @@ import (
 	"strings"
 )
 
-const PUBLIC_PEM = `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtIEgZLyg+tMgo08potRe
-dgxs98M+p29J6WXOiOOF50b/JF7bvDutEmBLAvRN3DkA2Pwhu1llDVzyXnVZjFkO
-br9/8NBr86NuHN9zT6mudF0hBViQlQa94lUp5I+CoZg3gB6238rkvXgG1S/TO9or
-3QrqbT1ZS1uU8hIqg9aV//QEeUtOfXNoETwt34DhLqzjXnNHyW24ODpYkIzgGpbV
-4RgitBExmrrAnnFMgfK7seWeu+H7s1ZME6aQ49DuxiFHXTAcG7zRO0m4PJ1yZTTF
-3abgThdEfY3BsFeP+E0zLSIPL2vvqLbEReWlZ4JqG59G91w+eAFZtpq4Mw4s3ihU
-SQIDAQAB
------END PUBLIC KEY-----`
-
 func Verify(signed_license string) (ok bool, err error) {
 	splits := strings.Split(signed_license, ".")
 	if len(splits) != 2 {
