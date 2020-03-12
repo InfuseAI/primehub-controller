d538aae23b51a698f3ed5ba5bf0b3b3a49c3663d
Timothy Lee <ctiml@infuseai.io>
2020-03-12 11:59:23 +0800

    Add a basic controller test for ImageSpec
    Signed-off-by: Timothy Lee <ctiml@infuseai.io>
    Co-authored-by: Ash Wu <hsatac@infuseai.io>

---
+++ controllers/imagespec_controller_test.go
@@ -0,0 +1,169 @@
+package controllers
+
+import (
+	"context"
+	"math/rand"
+	"path/filepath"
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	core "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	// +kubebuilder:scaffold:imports
+
+	primehubv1alpha1 "primehub-controller/api/v1alpha1"
+)
+
+// These tests use Ginkgo (BDD-style Go testing framework). Refer to
+// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
+
+var cfg *rest.Config
+var k8sClient client.Client
+var testEnv *envtest.Environment
+
+func TestAPIs(t *testing.T) {
+	RegisterFailHandler(Fail)
+
+	RunSpecsWithDefaultAndCustomReporters(t,
+		"Controller Suite",
+		[]Reporter{envtest.NewlineReporter{}})
+}
+
+var _ = BeforeSuite(func(done Done) {
+	logf.SetLogger(zap.LoggerTo(GinkgoWriter, true))
+
+	By("bootstrapping test environment")
+	testEnv = &envtest.Environment{
+		CRDDirectoryPaths: []string{filepath.Join("..", "config", "crd", "bases")},
+	}
+
+	var err error
+	cfg, err = testEnv.Start()
+	Expect(err).ToNot(HaveOccurred())
+	Expect(cfg).ToNot(BeNil())
+
+	err = primehubv1alpha1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	// +kubebuilder:scaffold:scheme
+
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	Expect(err).ToNot(HaveOccurred())
+	Expect(k8sClient).ToNot(BeNil())
+
+	close(done)
+}, 60)
+
+var _ = AfterSuite(func() {
+	By("tearing down the test environment")
+	err := testEnv.Stop()
+	Expect(err).ToNot(HaveOccurred())
+})
+
+func SetupTest(ctx context.Context) *core.Namespace {
+	var stopCh chan struct{}
+	ns := &core.Namespace{}
+
+	BeforeEach(func() {
+		stopCh = make(chan struct{})
+		*ns = core.Namespace{
+			ObjectMeta: metav1.ObjectMeta{Name: "testns-" + randStringRunes(5)},
+		}
+
+		err := k8sClient.Create(ctx, ns)
+		Expect(err).NotTo(HaveOccurred(), "failed to create test namespace")
+
+		mgr, err := ctrl.NewManager(cfg, ctrl.Options{})
+		Expect(err).NotTo(HaveOccurred(), "failed to create manager")
+
+		controller := &ImageSpecReconciler{
+			Client: mgr.GetClient(),
+			Log:    ctrl.Log.WithName("controllers").WithName("ImageSpec"),
+			Scheme: mgr.GetScheme(),
+		}
+		err = controller.SetupWithManager(mgr)
+		Expect(err).NotTo(HaveOccurred(), "failed to setup controller")
+
+		go func() {
+			err := mgr.Start(stopCh)
+			Expect(err).NotTo(HaveOccurred(), "failed to start manager")
+		}()
+	})
+
+	AfterEach(func() {
+		close(stopCh)
+
+		err := k8sClient.Delete(ctx, ns)
+		Expect(err).NotTo(HaveOccurred(), "failed to delete test namespace")
+	})
+
+	return ns
+}
+
+var _ = Context("Inside of a new namespace", func() {
+	ctx := context.TODO()
+	ns := SetupTest(ctx)
+
+	Describe("when no existing resources exist", func() {
+
+		It("should create a new ImageSpecJob resource after a new ImageSpec was created", func() {
+
+			now := metav1.Now()
+			imageSpec := &primehubv1alpha1.ImageSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "testresource",
+					Namespace: ns.Name,
+				},
+				Spec: primehubv1alpha1.ImageSpecSpec{
+					BaseImage: "jupyter/base-notebook",
+					Packages: primehubv1alpha1.ImageSpecSpecPackages{
+						Apt:   []string{"curl"},
+						Pip:   []string{},
+						Conda: []string{},
+					},
+					UpdateTime: &now,
+				},
+			}
+
+			err := k8sClient.Create(ctx, imageSpec)
+			Expect(err).NotTo(HaveOccurred(), "failed to create test ImageSpec resource")
+			job := &primehubv1alpha1.ImageSpecJob{}
+			Eventually(
+				getResourceFunc(ctx, client.ObjectKey{Name: imageSpec.Name + "-96ff7925", Namespace: imageSpec.Namespace}, job),
+				time.Second*5, time.Millisecond*500).Should(BeNil())
+
+			Expect(job.ObjectMeta.Labels["imagespecs.primehub.io/name"]).To(Equal(imageSpec.Name))
+		})
+	})
+})
+
+func getResourceFunc(ctx context.Context, key client.ObjectKey, obj runtime.Object) func() error {
+	return func() error {
+		return k8sClient.Get(ctx, key, obj)
+	}
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz1234567890")
+
+func randStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
