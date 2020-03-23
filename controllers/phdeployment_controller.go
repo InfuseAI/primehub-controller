e4ae8abd52fcb05bf6896a83c2853064da554779
ChanYiLin <j5111261112@gmail.com>
2020-03-23 16:55:17 +0800

    add phDeployment controller
    Signed-off-by: ChanYiLin <j5111261112@gmail.com>
    Signed-off-by: JackLin <chanyi.jack.lin@infuseai.io>
    Signed-off-by: ChanYiLin <j5111261112@gmail.com>

--- controllers/phdeployment_controller.go
+++ controllers/phdeployment_controller.go
@@ -17,12 +17,18 @@ package controllers
 
 import (
 	"context"
+	"time"
 
 	"github.com/go-logr/logr"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	primehubv1alpha1 "primehub-controller/api/v1alpha1"
+	seldonv1 "primehub-controller/seldon/apis/v1"
+
+	corev1 "k8s.io/api/core/v1"
 )
 
 // PhDeploymentReconciler reconciles a PhDeployment object
@@ -31,14 +37,137 @@ type PhDeploymentReconciler struct {
 	Log logr.Logger
 }
 
+func (r *PhDeploymentReconciler) buildSeldonDeployment(phDeployment primehubv1alpha1.PhDeployment) *seldonv1.SeldonDeployment {
+	// log := r.Log.WithValues("phDeployment", phDeployment.Name)
+
+	hash, _ := generateRandomString(6)
+	seldonDeploymentName := phDeployment.Name + "-" + hash
+
+	seldonDeployment := &seldonv1.SeldonDeployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        seldonDeploymentName,
+			Namespace:   phDeployment.Namespace,
+			Annotations: phDeployment.ObjectMeta.Annotations,
+			// Labels: map[string]string{
+			// 	"phjob.primehub.io/scheduledBy": phSchedule.Name,
+			// 	"primehub.io/group":             escapism.EscapeToPrimehubLabel(phSchedule.Spec.JobTemplate.Spec.GroupName),
+			// 	"primehub.io/user":              escapism.EscapeToPrimehubLabel(phSchedule.Spec.JobTemplate.Spec.UserName),
+			// },
+		},
+	}
+
+	// instanceInfo, err := r.getInstanceTypeInfo(phJob.Spec.InstanceType)
+	// resources := ConvertToResourceQuota(instanceInfo.Spec.LimitsCpu, (float32)(instanceInfo.Spec.LimitsGpu), instanceInfo.Spec.LimitsMemory)
+
+	componentSpecs := make([]*seldonv1.SeldonPodSpec, 0)
+
+	seldonPodSpec1 := &seldonv1.SeldonPodSpec{
+		Metadata: metav1.ObjectMeta{
+			Annotations: map[string]string{
+				"primehub.io/group":      phDeployment.Spec.GroupId,
+				"primehub.io/deployment": seldonDeploymentName,
+			},
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:  "model",
+					Image: phDeployment.predictors[0].modelImage,
+				},
+			},
+		},
+	}
+	componentSpecs = append(componentSpecs, seldonPodSpec1)
+
+	graph := &seldonv1.PredictiveUnit{
+		Name: "model",
+		Type: seldonv1.MODEL,
+		Endpoint: seldonv1.Endpoint{
+			Type: REST,
+		},
+	}
+
+	predictors := make([]seldonv1.PredictorSpec, 0)
+	predictor1 := &seldonv1.PredictorSpec{
+		Name:           "predictor1",
+		ComponentSpecs: componentSpecs,
+		Graph:          graph,
+		Replicas:       int32(1),
+		Annotation: map[string]string{
+			"predictor_version", "v1",
+		},
+	}
+	predictors = append(predictors, predictor1)
+
+	seldonDeployment.Spec.Predictors = predictors
+
+	return seldonDeployment
+
+}
+
+// func (r *PhDeploymentReconciler) getInstanceTypeInfo(instanceTypeId string) (*graphql.DtoInstanceType, error) {
+// 	cacheKey := "instanceType:" + instanceTypeId
+// 	cacheItem := InstanceTypeCache.Get(cacheKey)
+// 	if cacheItem == nil || cacheItem.Expired() {
+// 		instanceTypeInfo, err := r.GraphqlClient.FetchInstanceTypeInfo(instanceTypeId)
+// 		if err != nil {
+// 			return nil, err
+// 		}
+// 		InstanceTypeCache.Set(cacheKey, instanceTypeInfo, cacheExpiredTime)
+// 	}
+// 	return InstanceTypeCache.Get(cacheKey).Value().(*graphql.DtoInstanceType), nil
+// }
+
 // +kubebuilder:rbac:groups=primehub.io,resources=phdeployments,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=primehub.io,resources=phdeployments/status,verbs=get;update;patch
 
 func (r *PhDeploymentReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("phdeployment", req.NamespacedName)
+	ctx := context.Background()
+	log := r.Log.WithValues("phdeployment", req.NamespacedName)
 
-	// your logic here
+	phDeployment := &primehubv1alpha1.PhDeployment{}
+	if err := r.Get(ctx, req.NamespacedName, phDeployment); err != nil {
+		if apierrors.IsNotFound(err) {
+			log.Info("phDeployment deleted")
+			return ctrl.Result{}, nil
+		} else {
+			log.Error(err, "unable to fetch PhShceduleJob")
+		}
+	}
+
+	log.Info("Start Reconcile PhDeployment")
+	startTime := time.Now()
+	defer func() {
+		log.Info("Finished Reconciling phDeployment ", "phDeployment", phDeployment, "ReconcileTime", time.Since(startTime))
+	}()
+
+	phDeployment = phDeployment.DeepCopy()
+
+	if phDeployment.Spec.Stop == true {
+
+		// delete seldondeployment and ingress
+
+		phDeployment.Status.Phase = primehubv1alpha1.DeploymentStopped
+		if err := r.updatePhDeploymentStatus(ctx, phDeployment); err != nil {
+			return ctrl.Result{RequeueAfter: 1 * time.Minute}, err
+		}
+	}
+
+	// create seldon deployment
+	seldonDeployment, err = r.buildSeldonDeployment(phDeployment)
+	if err == nil {
+		err = r.Client.Create(seldonDeployment)
+	}
+
+	if err == nil {
+		log.Info("seldonDeployment created", "seldonDeployment", seldonDeployment)
+	} else {
+		log.Error(err, "creating seldonDeployment failed")
+	}
+
+	// create ingress
+
+	// update status based on seldon deployment
 
 	return ctrl.Result{}, nil
 }
@@ -47,4 +176,18 @@ func (r *PhDeploymentReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&primehubv1alpha1.PhDeployment{}).
 		Complete(r)
+}
+
+// updatePhDeploymentStatus update the status of the phDeployment in the cluster.
+func (r *PhDeploymentReconciler) updatePhDeploymentStatus(ctx context.Context, phDeployment *primehubv1alpha1.PhDeployment) error {
+	log := r.Log.WithValues("phDeployment", phDeployment.Name)
+	updateTime := time.Now()
+	defer func() {
+		log.Info("Finished updating PhDeployment ", "UpdateTime", time.Since(updateTime))
+	}()
+	if err := r.Status().Update(ctx, phDeployment); err != nil {
+		log.Error(err, "failed to update PhDeployment status")
+		return err
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,23 +3,27 @@ module primehub-controller
 go 1.13
 
 require (
+	github.com/Azure/go-autorest v11.1.2+incompatible // indirect
+	github.com/coreos/etcd v3.3.13+incompatible // indirect
 	github.com/go-logr/logr v0.1.0
 	github.com/karlseguin/ccache v2.0.3+incompatible
 	github.com/magiconair/properties v1.8.1 // indirect
-	github.com/onsi/ginkgo v1.6.0
-	github.com/onsi/gomega v1.4.2
+	github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
+	github.com/onsi/ginkgo v1.11.0
+	github.com/onsi/gomega v1.8.1
 	github.com/pelletier/go-toml v1.6.0 // indirect
 	github.com/prometheus/common v0.4.1
 	github.com/robfig/cron/v3 v3.0.0
+	github.com/seldonio/seldon-core/operator v0.0.0-20200322144723-20ef400732e7
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.4.0
-	github.com/stretchr/testify v1.3.0
+	github.com/stretchr/testify v1.4.0
 	go.uber.org/zap v1.10.0
 	golang.org/x/sys v0.0.0-20191029155521-f43be2a4598c // indirect
-	golang.org/x/text v0.3.2 // indirect
-	k8s.io/api v0.0.0-20190918195907-bd6ac527cfd2
-	k8s.io/apimachinery v0.0.0-20190817020851-f2f3a405f61d
-	k8s.io/client-go v0.0.0-20190918200256-06eb1244587a
-	sigs.k8s.io/controller-runtime v0.3.0
+	gopkg.in/yaml.v1 v1.0.0-20140924161607-9f9df34309c0 // indirect
+	k8s.io/api v0.17.2
+	k8s.io/apimachinery v0.17.2
+	k8s.io/client-go v0.17.2
+	sigs.k8s.io/controller-runtime v0.5.0
+	sigs.k8s.io/testing_frameworks v0.1.1 // indirect
 )
