9421fb0f6bac5d4ee628dc3217aa049c9cd29e52
ChanYiLin <j5111261112@gmail.com>
2020-04-21 10:56:12 +0800

    remove the space in the begining of the info
    Signed-off-by: ChanYiLin <j5111261112@gmail.com>
    Signed-off-by: Jack Lin <chanyi.jack.lin@infuseai.io>

--- ee/controllers/phdeployment_controller.go
+++ ee/controllers/phdeployment_controller.go
@@ -301,7 +301,7 @@ func (r *PhDeploymentReconciler) updateStatus(ctx context.Context, phDeployment 
 		if c.Type == v1.DeploymentReplicaFailure && c.Status == corev1.ConditionTrue && c.Reason == "FailedCreate" {
 			// create replicaset failed, because request exceeds quota and is denied by admission webhook
 			phDeployment.Status.Phase = primehubv1alpha1.DeploymentDeploying
-			phDeployment.Status.Messsage = strings.Split(c.Message, "denied the request:")[1]
+			phDeployment.Status.Messsage = strings.Split(c.Message, "denied the request: ")[1]
 			phDeployment.Status.Replicas = int(deployment.Status.Replicas)
 			phDeployment.Status.AvailableReplicas = int(deployment.Status.AvailableReplicas)
 
